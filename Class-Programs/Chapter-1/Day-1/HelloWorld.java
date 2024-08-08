e1f6a8faa383adde2e66bcb9d54b7c429f46df96
Sheewakoti Manish <87434771+hunter-devil2057@users.noreply.github.com>
2024-08-08 17:57:27 +0545

    Basics of Java - Printing Outputs

---
+++ Class-Programs/Chapter-1/Day-1/HelloWorld.java
@@ -0,0 +1,13 @@
+package chapter.pkg1;
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, World!");
+//        System.out.println() prints in new line, by breaking the line
+
+        System.out.print("Hey, ");
+        System.out.print("Printing Output in same Line\n");
+//        System.out.print("") prints the output without breaking the line  
+    }
+}
