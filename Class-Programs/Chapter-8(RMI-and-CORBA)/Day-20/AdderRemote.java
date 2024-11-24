0deaa699e10d33de0b000dfa287e0299011c7cab
hunter-devil2057 <manishshiwakoti42@gmail.com>
2024-11-24 22:06:39 +0545

    Implementation of Class (Remote Object)

---
+++ Class-Programs/Chapter-8(RMI-and-CORBA)/Day-20/AdderRemote.java
@@ -0,0 +1,14 @@
+//package chapter8;
+//import Adder.java;
+public class AdderRemote implements Adder
+{
+//    Creating Constructor for throwing when Remote Exception Occurs
+    AdderRemote()
+    {
+        super();
+    }
+    public int add(int a, int b)
+    {
+        return a+b;
+    }
+}
